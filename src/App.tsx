c241819956a8f463fa4f4125470f463fa1f910ef
Andrew Workman <andrew.workman@missionlabs.co.uk>
2024-07-29 07:44:16 +0100

    feat: Adds react-router and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import NavBar from './components/Nav';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs';
-
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
-  return (
-    <>
-        <NavBar />
-        <Hero title='Test Title' subTitle='This is the Substitle' />
-        <HomeCards />
-        <JobListings />
-        <ViewAllJobs />
-    </>
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout/>}>
+        <Route index element={<HomePage />} />)
+    </Route>
+  ))
+
+  return <RouterProvider router={router} />;
 };
 
-export default App
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+// import NavBar from './components/Nav';
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards'
+import JobListings from '../components/JobListings'
+import ViewAllJobs from '../components/ViewAllJobs';
+
+const HomePage = () => {
+    return (
+        <>
+        <Hero title="Become a React Developer" subTitle="Find a React job that suits your skill set" />
+        <HomeCards />
+        <JobListings />
+        <ViewAllJobs />
+        </>
+    )
+}
+
+export default HomePage
