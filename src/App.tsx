4f461a8135ab73fa77e7487d21d6b262b9375e8c
Andrew Workman <andrew.workman@missionlabs.co.uk>
2024-07-31 09:12:10 +0100

    feat: Add new job and delete job

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,44 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage"
 import NotFoundPage  from './pages/NotFoundPage'
+import JobPage from './pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
+import { Job } from './types'
 
 const App = () => {
+
+  const addJob = async(newJob: Partial<Job> ) => {
+    console.log(newJob)
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify(newJob)
+    })
+    console.log('res :', res);
+    return;
+  }
+
+  const deleteJob = async(id: string) =>{
+    console.log("delete", id)
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+      headers : {
+        'Content-Type' : 'application/json'
+      }
+    })
+    console.log('res :', res);
+    return
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout/>}>
         <Route index element={<HomePage />} />
-        <Route path='jobs' element={<JobsPage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} />
+        <Route path='/add-job/' element={<AddJobPage addJobSubmit={addJob} />} />
         <Route path='*' element={<NotFoundPage />} />
     </Route>
   ))
--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -43,7 +43,7 @@ const JobListing: FC<Props> = ({ job }) => {
             {job.location}
           </div>
           <Link
-            to={`/job/${job.id}`}
+            to={`/jobs/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
